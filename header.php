d569e97f55d4291ec69c3360f7ca28fe160735de
Mohamad3212 <d312093@edu.curio.nl>
2025-04-03 12:09:21 +0200

    header aanpassing

---
+++ header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/main.css">
+<header>
+    <div class="container">
+        <h1>Takenlijst</h1>
+        <nav>
+            <a href="">Main</a>
+            <a href="">Footer</a>
+            <a href="">Takenlijst</a>
+        </nav>
+    </div>
+</header>
